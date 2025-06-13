97b2cb97f35000b8867884ff7d32a8a667e76ede
devark28 <e.shimwa1@alustudent.com>
2025-06-13 15:48:24 +0200

    Feat: store the original sms text

--- backend/database/tableInserstionQueries.js
+++ backend/database/tableInserstionQueries.js
@@ -1,85 +1,82 @@
 import {formatMySQLDateTime} from "../utils/dateFormatter.js";
 
 export function createIncomingInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, sender=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, sender=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO incoming (transactionId, type, amount, timestamp, currency, sender)
-            VALUES (?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, sender]
+    query: `INSERT INTO incoming (transactionId, type, amount, timestamp, currency, sender, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, sender, originalSMS]
   };
 }
 
 export function createReclaimedInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, sender=null, senderNumber=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, sender=null, senderNumber=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO reclaimed (transactionId, type, amount, timestamp, currency, sender, senderNumber)
-            VALUES (?, ?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, sender, senderNumber]
+    query: `INSERT INTO reclaimed (transactionId, type, amount, timestamp, currency, sender, senderNumber, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, sender, senderNumber, originalSMS]
   };
 }
 
 export function createPaymentInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, recipient=null, recipientNumber=null, fee=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, recipient=null, recipientNumber=null, fee=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO payment (transactionId, type, amount, timestamp, currency, recipient, recipientNumber, fee)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, recipient, recipientNumber, fee]
+    query: `INSERT INTO payment (transactionId, type, amount, timestamp, currency, recipient, recipientNumber, fee, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, recipient, recipientNumber, fee, originalSMS]
   };
 }
 
 export function createBankDepositInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO bank_deposit (transactionId, type, amount, timestamp, currency)
-            VALUES (?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency]
+    query: `INSERT INTO bank_deposit (transactionId, type, amount, timestamp, currency, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, originalSMS]
   };
 }
 
 export function createTransferInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, recipient=null, recipientNumber=null, fee=null}} = data;
-  // details: {currency: 'RWF', recipient: 'Linda Green', recipientNumber: '250795963036'}
+  const {transactionId, type, amount, timestamp, details: {currency=null, recipient=null, recipientNumber=null, fee=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO transfer (transactionId, type, amount, timestamp, currency, recipient, recipientNumber, fee)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, recipient, recipientNumber, fee]
+    query: `INSERT INTO transfer (transactionId, type, amount, timestamp, currency, recipient, recipientNumber, fee, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, recipient, recipientNumber, fee, originalSMS]
   };
 }
 
 export function createWithdrawnInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, agent=null, agentNumber=null, fee=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, agent=null, agentNumber=null, fee=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO withdrawn (transactionId, type, amount, timestamp, currency, agent, agentNumber, fee)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, agent, agentNumber, fee]
+    query: `INSERT INTO withdrawn (transactionId, type, amount, timestamp, currency, agent, agentNumber, fee, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, agent, agentNumber, fee, originalSMS]
   };
 }
 
 export function createAirtimeBillInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, purchasedItem=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, purchasedItem=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO airtime_bill (transactionId, type, amount, timestamp, currency, purchasedItem)
-            VALUES (?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, purchasedItem]
+    query: `INSERT INTO airtime_bill (transactionId, type, amount, timestamp, currency, purchasedItem, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, purchasedItem, originalSMS]
   };
 }
 
 export function createUtilityBillInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, purchasedUtility=null, meterToken=null, fee=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, purchasedUtility=null, meterToken=null, fee=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO utility_bill (transactionId, type, amount, timestamp, currency, purchasedUtility, meterToken,
-                                      fee)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, purchasedUtility, meterToken, fee]
+    query: `INSERT INTO utility_bill (transactionId, type, amount, timestamp, currency, purchasedUtility, meterToken, fee, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, purchasedUtility, meterToken, fee, originalSMS]
   };
 }
 
 export function createThirdPartyInsertQuery(data) {
-  const {transactionId, type, amount, timestamp, details: {currency=null, thirdParty=null, fee=null, externalTransactionId=null}} = data;
+  const {transactionId, type, amount, timestamp, details: {currency=null, thirdParty=null, fee=null, externalTransactionId=null, originalSMS=null}} = data;
   return {
-    query: `INSERT INTO third_party (transactionId, type, amount, timestamp, currency, thirdParty, fee,
-                                     externalTransactionId)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
-    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, thirdParty, fee, externalTransactionId]
+    query: `INSERT INTO third_party (transactionId, type, amount, timestamp, currency, thirdParty, fee, externalTransactionId, originalSMS)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    values: [transactionId, type, amount, formatMySQLDateTime(timestamp), currency, thirdParty, fee, externalTransactionId, originalSMS]
   };
 }
